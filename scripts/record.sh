12d7ab60b64c9bbb524fbdda5f9b939c80904713
Your Name <youremail@example.com>
2024-04-20 13:43:02 +0200

    Updated record script

--- scripts/record.sh
+++ scripts/record.sh
@@ -19,59 +19,11 @@ exclude=(
 # Every topic containing "compressed"
 '(.*)compressed(.*)'
 # Every topic containing "image_raw"
-# '(.*)image_raw(.*)'
+'(.*)image_raw(.*)'
 # Every topic containing "theora"
 '(.*)theora(.*)'
 # Every topic containing "h264"
 '(.*)h264(.*)'
-
-'(.*)/bluefox_front/camera_info'
-'(.*)/bluefox_front/image_raw'
-'(.*)/bluefox_front/parameter_descriptions'
-'(.*)/bluefox_front/parameter_updates'
-
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressed'
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressed/parameter_descriptions'
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressed/parameter_updates'
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressedDepth'
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressedDepth/parameter_descriptions'
-'(.*)/blink_processor_new/uvdar_blink_visualization/compressedDepth/parameter_updates'
-'(.*)/blink_processor_new/uvdar_blink_visualization/theora'
-'(.*)/blink_processor_new/uvdar_blink_visualization/theora/parameter_descriptions'
-'(.*)/blink_processor_new/uvdar_blink_visualization/theora/parameter_updates'
-'(.*)/uvdar_bluefox/left/camera_info'
-'(.*)/uvdar_bluefox/left/image_raw'
-'(.*)/uvdar_bluefox/left/image_raw/compressed'
-'(.*)/uvdar_bluefox/left/image_raw/compressed/parameter_descriptions'
-'(.*)/uvdar_bluefox/left/image_raw/compressed/parameter_updates'
-'(.*)/uvdar_bluefox/left/image_raw/compressedDepth'
-'(.*)/uvdar_bluefox/left/image_raw/compressedDepth/parameter_descriptions'
-'(.*)/uvdar_bluefox/left/image_raw/compressedDepth/parameter_updates'
-'(.*)/uvdar_bluefox/left/image_raw/theora'
-'(.*)/uvdar_bluefox/left/image_raw/theora/parameter_descriptions'
-'(.*)/uvdar_bluefox/left/image_raw/theora/parameter_updates'
-'(.*)/uvdar_bluefox/right/camera_info'
-'(.*)/uvdar_bluefox/right/image_raw'
-'(.*)/uvdar_bluefox/right/image_raw/compressed'
-'(.*)/uvdar_bluefox/right/image_raw/compressed/parameter_descriptions'
-'(.*)/uvdar_bluefox/right/image_raw/compressed/parameter_updates'
-'(.*)/uvdar_bluefox/right/image_raw/compressedDepth'
-'(.*)/uvdar_bluefox/right/image_raw/compressedDepth/parameter_descriptions'
-'(.*)/uvdar_bluefox/right/image_raw/compressedDepth/parameter_updates'
-'(.*)/uvdar_bluefox/right/image_raw/theora'
-'(.*)/uvdar_bluefox/right/image_raw/theora/parameter_descriptions'
-'(.*)/uvdar_bluefox/right/image_raw/theora/parameter_updates'
-# '(.*)/uvdar_bluefox/back/camera_info'
-# '(.*)/uvdar_bluefox/back/image_raw'
-# '(.*)/uvdar_bluefox/back/image_raw/compressed'
-# '(.*)/uvdar_bluefox/back/image_raw/compressed/parameter_descriptions'
-# '(.*)/uvdar_bluefox/back/image_raw/compressed/parameter_updates'
-# '(.*)/uvdar_bluefox/back/image_raw/compressedDepth'
-# '(.*)/uvdar_bluefox/back/image_raw/compressedDepth/parameter_descriptions'
-# '(.*)/uvdar_bluefox/back/image_raw/compressedDepth/parameter_updates'
-# '(.*)/uvdar_bluefox/back/image_raw/theora'
-# '(.*)/uvdar_bluefox/back/image_raw/theora/parameter_descriptions'
-# '(.*)/uvdar_bluefox/back/image_raw/theora/parameter_updates'
 )
 
 # file's header
@@ -80,7 +32,7 @@ echo "<launch>" > "$filename"
 echo "<arg name=\"UAV_NAME\" default=\"\$(env UAV_NAME)\" />" >> "$filename"
 echo "<group ns=\"\$(arg UAV_NAME)\">" >> "$filename"
 
-echo -n "<node pkg=\"mrs_uav_general\" type=\"mrs_record\" name=\"mrs_rosbag_record\" output=\"screen\" args=\"-o $path -a" >> "$filename"
+echo -n "<node pkg=\"rosbag\" type=\"record\" name=\"rosbag_record\" output=\"screen\" args=\"-o $path -a" >> "$filename"
 
 # if there is anything to exclude
 if [ "${#exclude[*]}" -gt 0 ]; then
