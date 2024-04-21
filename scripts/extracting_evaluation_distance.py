4111f3bbe11ec463d5099450d9cf2f7e113b2647
Your Name <youremail@example.com>
2024-04-21 13:24:15 +0200

    Modified extracting data script, and some launch files update

--- scripts/extracting_evaluation_distance.py
+++ scripts/extracting_evaluation_distance.py
@@ -6,9 +6,40 @@ import os
 
 #bag_path = os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/test_standard_topics_3.bag')
 
-# List of ROS bag files
+
+primary_bag_path = os.path.expanduser('~/bag_files/marlon_experiments_temesvar/uav38_kl_first/uav38_kl_first.bag')  
+secondary_bag_path = os.path.expanduser('~/bag_files/marlon_experiments_temesvar/uav39_kl_first/uav39_kl_first.bag') 
+
+
+output_csv = os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/rw/raw_csv/kl_exp1.csv')
+
+distance_y_list = []
+with rosbag.Bag(secondary_bag_path, 'r') as bag:
+    for topic, msg, t in bag.read_messages(topics=['/uav39/control_manager/control_reference']):
+        distance_y_list.append(msg.pose.pose.position.y)
+
+
+#Print size of distance_y_list
+print(len(distance_y_list))
+
+combined_data = []
+index = 0  
+with rosbag.Bag(primary_bag_path, 'r') as bag:
+    for topic, msg, t in bag.read_messages(topics=['/uav38/uvdar/blinkers_seen_left', '/uav38/uvdar/blinkers_seen_right']):
+        timestamp = t.to_sec()
+        distance_x = distance_y_list[index] if index < len(distance_y_list) else None 
+        entry = {'timestamp': timestamp, 'distance_y': distance_x, 'value': None}
+        if topic in ['/uav38/uvdar/blinkers_seen_left', '/uav38/uvdar/blinkers_seen_right']:
+            entry['value'] = [point.value for point in msg.points]
+        combined_data.append(entry)
+        index += 1  
+
+
+
+
+""" # List of ROS bag files
 bag_files = [
-    os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/multi/otsu_multiple_topics.bag'),
+    os.path.expanduser('~/bag_files/marlon_experiments_temesvar/otsu_first/otsu_first.bag'),
 
     #os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/static_standard_topics_8m.bag'),
     #os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/static_standard_topics_11m.bag'),
@@ -21,10 +52,10 @@ bag_files = [
 #output_csv = os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/standard_test_3.csv')
 
 # Output CSV file
-output_csv = os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/simulation/raw_csv/otsu_multiple_topics.csv')
+output_csv = os.path.expanduser('~/Desktop/MRS_Master_Project/rosbags/rw/raw_csv/otsu_exp1.csv')
 
 # Define the topics to extract
-topics = ['/uav1/uvdar/blinkers_seen_left','/uav1/uvdar/blinkers_seen_right','/uav1/control_manager/control_reference']
+topics = ['/uav38/uvdar/blinkers_seen_left','/uav38/uvdar/blinkers_seen_right','/uav38/control_manager/control_reference']
 
 
 combined_data = []
@@ -35,22 +66,22 @@ for bag_path in bag_files:
         data = []  # Temporary list to store data from the current bag
         for topic, msg, t in bag.read_messages(topics=topics):
             entry = {'timestamp': t.to_sec(), 'distance_x': None, 'value': None}
-            if topic in ['/uav1/uvdar/blinkers_seen_left', '/uav1/uvdar/blinkers_seen_right']:
+            if topic in ['/uav38/uvdar/blinkers_seen_left', '/uav38/uvdar/blinkers_seen_right']:
                 #entry['value'] = msg.points[0].value if msg.points else None
                 #Take all the values
                 entry['value'] = [point.value for point in msg.points]
-            elif topic == '/uav1/control_manager/control_reference':
+            elif topic == '/uav38/control_manager/control_reference':
                 # Extract the x position as distance
                 entry['distance_x'] = msg.pose.pose.position.x
             data.append(entry)
-        combined_data.extend(data)  # Add the data from the current bag to the combined list
+        combined_data.extend(data)  # Add the data from the current bag to the combined list """
 
 # Sort combined data by timestamp to align the entries
 combined_data.sort(key=lambda x: x['timestamp'])
 
 # Write the combined data to a CSV file
 with open(output_csv, 'w', newline='') as csvfile:
-    fieldnames = ['timestamp', 'distance_x', 'value']
+    fieldnames = ['timestamp', 'distance_y', 'value']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
